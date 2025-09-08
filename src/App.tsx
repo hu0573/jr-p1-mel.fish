a97bb73c275a39b71c334881576571a3637ccb01
Ruiqing Hu <huruiqing0011@gmail.com>
2025-09-08 13:02:31 +0930

    section04

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Section03 from "./Section03/Section03.tsx";
-import TitleWidthCircle from "./TitleWidthCircle/TitleWidthCircle.tsx";
+import { Section04 } from "./Section04/Section04.tsx";
 
 function App() {
   return (
     <div className="p-8">
       <Section03 />
+      <Section04 />
     </div>
   );
 }
