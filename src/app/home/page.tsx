11c0673b5645225ee00e80fe1016844177946887
Ruiqing Hu <huruiqing0011@gmail.com>
2025-09-18 09:02:16 +0930

    modify bannerCaseStudiesBanner

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 import HeroHeader from "./_components/HeroHeader";
 import ProjectShowcase from "./_components/ProjectShowcase";
-import { CaseStudiesBanner } from "./_components/CaseStudiesBanner";
+import CaseStudiesBanner from "./_components/CaseStudiesBanner";
 import AudienceSegments from "./_components/AudienceSegments";
 import CallToActionWave from "./_components/CallToActionWave";
 import SiteFooter from "./_components/SiteFooter";
