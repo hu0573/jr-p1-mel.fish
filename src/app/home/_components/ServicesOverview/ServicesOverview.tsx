4902630c859fbcef0d88e81b4e649a3f8cca4446
Tina Luo <yuhanluo30@gmail.com>
2025-09-17 15:45:19 +1000

    Adjust the spacing, position

--- src/app/home/_components/ServicesOverview/ServicesOverview.tsx
+++ src/app/home/_components/ServicesOverview/ServicesOverview.tsx
@@ -1,4 +1,3 @@
-import logo from "./_assets/Ellipse_6204.png";
 import background1 from "./_assets/Mask_group4.png";
 import background2 from "./_assets/Mask_group6.png";
 import background3 from "./_assets/Mask_group7.png";
@@ -9,6 +8,7 @@ import block1 from "./_assets/image_12974.png";
 import block2 from "./_assets/image_12975.png";
 import block3 from "./_assets/image_12976.png";
 import block4 from "./_assets/image_12977.png";
+import TitleWithCircle from "../../../../_components/TitleWithCircle";
 
 const button_color = "#2D7597";
 const background = "#F7F7F7";
@@ -16,13 +16,13 @@ const label = "#FD933F";
 
 const FF = "PingFang SC, PingFang SC";
 
+const title = "Services";
 const small_title = "3.5rem";
 const section = "2.625rem";
 const paragraph = "1.125rem";
-const logoSize = "2.25rem";
 const button = "1rem";
-const PAGE_PADDING = "px-[40px]";
-const PAGE_MAXW = "max-w-[1360px]";
+const PAGE_PADDING = "px-4 sm:px-6 lg:px-8";
+const PAGE_MAXW = "max-w-6xl";
 const PAGE_SPACING_Y = "py-12 lg:py-16";
 
 function ServicesOverview() {
@@ -36,23 +36,12 @@ function ServicesOverview() {
         backgroundColor: background,
       }}
     >
-      <div
-        className={`mx-auto ${PAGE_MAXW} ${PAGE_PADDING} ${PAGE_SPACING_Y}`}
-      >
+      <div className={`mx-auto ${PAGE_MAXW} ${PAGE_PADDING} ${PAGE_SPACING_Y}`}>
         <div className="mb-20">
-          <div
-            className="flex bg-no-repeat mb-1"
-            style={{
-              backgroundImage: `url(${logo})`,
-              backgroundPosition: "left 48%",
-              backgroundSize: "4%",
-            }}
-          >
-            <h3 className="pl-5 pt-5 font-bold" style={{ fontSize: logoSize }}>
-              Services
-            </h3>
+          <div className="mb-[88px]">
+            <TitleWithCircle text={title} />
           </div>
-          <div className="pt-5 mt-10 mb-11">
+          <div className="mb-[124px]">
             <h1
               className="font-bold max-w-4xl"
               style={{ fontSize: small_title, fontFamily: FF }}
@@ -60,13 +49,14 @@ function ServicesOverview() {
               Building with Precision,Delivering with lmpact.
             </h1>
             <h3
-              className="mb-5 mt-5"
+              className="mt-4 mb-5"
               style={{ fontSize: paragraph, fontFamily: FF }}
             >
-              We provide end-to-end solutions to bring your digital ideas to life.
+              We provide end-to-end solutions to bring your digital ideas to
+              life.
             </h3>
             <button
-              className="flex items-center mt-10 space-x-3 py-2.5 px-4 rounded-full hover:bg-button_hover max-h-14"
+              className="flex items-center mt-8 space-x-3 py-2.5 px-4 rounded-full hover:bg-button_hover max-h-14"
               style={{ backgroundColor: button_color }}
             >
               <h5
@@ -78,15 +68,16 @@ function ServicesOverview() {
             </button>
           </div>
         </div>
-        <section className="space-y-20">
+        <section className="space-y-12">
           <div
             className="flex w-full py-2 bg-cover bg-no-repeat justify-between items-center"
             style={{
               backgroundImage: `url(${background4})`,
               backgroundSize: "auto 100%",
+              backgroundPosition: "left center",
             }}
           >
-            <div className="grid gap-3">
+            <div className="grid gap-3 pl-[42px]">
               <h3
                 className="font-bold bg-cover bg-no-repeat"
                 style={{
@@ -103,8 +94,8 @@ function ServicesOverview() {
                 className="w-4/5"
                 style={{ fontSize: paragraph, fontFamily: FF }}
               >
-                Expert guidance on architecture, tech stack, and bestpractices for
-                building robust and scalable applications
+                Expert guidance on architecture, tech stack, and bestpractices
+                for building robust and scalable applications
               </h5>
               <div
                 className="flex gap-3"
@@ -133,7 +124,7 @@ function ServicesOverview() {
             <img
               src={block1}
               alt="Full-stack Consulting"
-              className="self-center w-64 h-64"
+              className="self-center w-64 h-64 transform -translate-x-15"
             ></img>
           </div>
 
@@ -142,16 +133,16 @@ function ServicesOverview() {
             style={{
               backgroundImage: `url(${background5})`,
               backgroundSize: "auto 100%",
-              backgroundPosition: "right",
+              backgroundPosition: "right center",
             }}
           >
             <img
               src={block2}
               alt="Full-stack Consulting"
-              className="self-center ml-20"
-              style={{ height: "60%" }}
+              className="self-center ml-20 translate-y-5"
+              style={{ width: "11rem" }}
             ></img>
-            <div className="grid text-right gap-3">
+            <div className="grid text-right gap-3 pr-[42px] py-[42px]">
               <h3
                 className="font-bold bg-cover bg-no-repeat"
                 style={{
@@ -202,9 +193,10 @@ function ServicesOverview() {
             style={{
               backgroundImage: `url(${background4})`,
               backgroundSize: "auto 100%",
+              backgroundPosition: "left center",
             }}
           >
-            <div className="grid gap-3">
+            <div className="grid gap-3 pl-[42px]">
               <h3
                 className="font-bold bg-cover bg-no-repeat"
                 style={{
@@ -251,26 +243,26 @@ function ServicesOverview() {
             <img
               src={block3}
               alt="Full-stack Consulting"
-              className="self-center"
-              style={{ height: "30%" }}
+              className="self-center transform -translate-x-38 translate-y-5"
+              style={{ width: "9rem" }}
             ></img>
           </div>
 
           <div
-            className="flex w-full h-64 pt-3 py-18 pb-5 bg-cover bg-no-repeat justify-between items-center"
+            className="flex w-full py-3 bg-cover bg-no-repeat justify-between items-center"
             style={{
               backgroundImage: `url(${background5})`,
               backgroundSize: "auto 100%",
-              backgroundPosition: "right",
+              backgroundPosition: "right center",
             }}
           >
             <img
               src={block4}
               alt="Full-stack Consulting"
-              className="self-center ml-10"
-              style={{ height: "120%" }}
+              className="self-center ml-20"
+              style={{ width: "11rem" }}
             ></img>
-            <div className="grid text-right gap-3">
+            <div className="grid text-right gap-3 pr-[42px] py-[42px]">
               <h3
                 className="font-bold bg-cover bg-no-repeat"
                 style={{
