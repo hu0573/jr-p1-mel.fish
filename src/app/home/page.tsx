e61263f261c6dbdea1421ddf1c953deae7de19e0
Tina Luo <yuhanluo30@gmail.com>
2025-09-17 14:33:18 +1000

    use TitleWithCircle

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,10 +4,12 @@ import AudienceSegments from "./_components/AudienceSegments";
 import CallToActionWave from "./_components/CallToActionWave";
 import SiteFooter from "./_components/SiteFooter";
 import HomeLayout from "./layout";
+import ServicesOverview from "./_components/ServicesOverview";
 
 export default function HomePage() {
   return (
     <HomeLayout>
+      <ServicesOverview />
       <ProjectShowcase />
       <CaseStudiesBanner />
       <AudienceSegments />
